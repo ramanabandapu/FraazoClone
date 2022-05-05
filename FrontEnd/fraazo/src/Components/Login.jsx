1f90d1ca7d2b6f78e32516e33d169bbf47230ef4
swatibersurda <swatibersurda@gmail.com>
2022-05-05 21:39:16 +0530

    adding details

---
+++ FrontEnd/fraazo/src/Components/Login.jsx
@@ -0,0 +1,60 @@
+import { React, useState } from 'react'
+import axios from "axios"
+import "./Login.css"
+import { useNavigate } from 'react-router-dom'
+
+export const Login = ({ setLoginuser }) => {
+  const navigate = useNavigate()
+  const [user, setUser] = useState({
+   mobileno:""
+  })
+  const [getid,setGetid]=useState()
+
+  const handlechange = (e) => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+    // console.log(value)
+  }
+
+  const login = async () => {
+    
+
+    axios.post(" http://localhost:8080/user", user)
+      .then(res => {
+       console.log(res.data);
+        alert(res.data.message)
+        // setLoginuser(res.data.user)
+        let msg = res.data.message
+        console.log(res.data.user)
+        console.log(msg)
+        if (msg === "Login Successfull") {
+
+          navigate("/product")
+        }
+        else {
+          navigate("/login")
+        }
+      }
+      )
+
+
+
+  }
+
+
+
+      
+
+
+
+  return (
+    <div className='login'>
+      <h1>Login</h1>
+      <input type="number" name="mobileno" value={user.mobileno} onChange={handlechange} placeholder='Enter Mobile Number' />
+      {/* <input type="text" name="password" value={user.password} onChange={handlechange} placeholder='Enter Password' /> */}
+      <button onClick={login} className='logbutton'>Login</button>
+      <div>or</div>
+      <button onClick={() => navigate("/register")} className='regsbutton'>Register</button>
+    </div>
+  )
+}
